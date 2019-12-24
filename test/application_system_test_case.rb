56521a5bc557d5a21542eee64156e2e3927dfc79
Colin Fleming <c3flemin@gmail.com>
2019-12-24 00:20:08 -0500

    more junk that doesn't work

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -8,7 +8,10 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
 
   before { Capybara.reset_sessions! }
 
+  Capybara.always_include_port = true
   # if in CI, run system tests headlessly.
-  browser = ENV['GITHUB_WORKFLOW'] ? :headless_chrome : :chrome
-  driven_by :selenium, using: browser
+  # browser = ENV['GITHUB_WORKFLOW'] ? :headless_chrome : :chrome
+  browser = :headless_chrome
+  opts = { url: "http://myapp-selenium:4444/wd/hub" }
+  driven_by :selenium, using: browser, options: opts
 end
